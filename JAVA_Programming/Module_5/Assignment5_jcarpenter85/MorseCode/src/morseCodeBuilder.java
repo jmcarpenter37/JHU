2507d38cc4a1d6f7469aa2093fe9b92df33be403
montagne <blackdog0x41@outlook.com>
2020-06-19 15:17:24 -0400

    Morse to english

--- JAVA_Programming/Module_5/Assignment5_jcarpenter85/MorseCode/src/morseCodeBuilder.java
+++ JAVA_Programming/Module_5/Assignment5_jcarpenter85/MorseCode/src/morseCodeBuilder.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-public class morseCodeBuilder
-{
+public class morseCodeBuilder {
     /*
     This project involves writing a program to translate Morse code
     into English and English into Morse code. Your program shall prompt
@@ -10,59 +9,158 @@ public class morseCodeBuilder
     results.  The Morse code pattern and English letter translations must be kept
     and processed using either two one-dimensional or one two-dimensional arrays.
      */
-    public static void morse_to_english(String val)
-    {
-        int i;
+    public static void morse_to_english(String val) {
         /*
         The user will pass in morse code and the
         morse code is translated to english
          */
-
-        if (val.contentEquals("--*"))
-        {
-            System.out.print("A");
+        //initialize String arrays
+        String alphabet[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " | "};
+        String nums[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+        // Alphabet cases first then the nums second
+        if (val.contentEquals(".-")) {
+            System.out.print(alphabet[0]);
         }
-        else
-            {
-                System.out.print(" | ");
-
+        if (val.contentEquals("-...")) {
+            System.out.print(alphabet[1]);
+        }
+        if (val.contentEquals("-.-.")) {
+            System.out.print(alphabet[2]);
+        }
+        if (val.contentEquals("-..")) {
+            System.out.print(alphabet[3]);
+        }
+        if (val.contentEquals(".")) {
+            System.out.print(alphabet[4]);
+        }
+        if (val.contentEquals("..-.")) {
+            System.out.print(alphabet[5]);
+        }
+        if (val.contentEquals("--.")) {
+            System.out.print(alphabet[6]);
+        }
+        if (val.contentEquals("....")) {
+            System.out.print(alphabet[7]);
+        }
+        if (val.contentEquals("..")) {
+            System.out.print(alphabet[8]);
+        }
+        if (val.contentEquals(".---")) {
+            System.out.print(alphabet[9]);
+        }
+        if (val.contentEquals("-.-")) {
+            System.out.print(alphabet[10]);
+        }
+        if (val.contentEquals(".-..")) {
+            System.out.print(alphabet[11]);
+        }
+        if (val.contentEquals("--")) {
+            System.out.print(alphabet[12]);
+        }
+        if (val.contentEquals("-.")) {
+            System.out.print(alphabet[13]);
+        }
+        if (val.contentEquals("---")) {
+            System.out.print(alphabet[14]);
+        }
+        if (val.contentEquals(".--.")) {
+            System.out.print(alphabet[15]);
+        }
+        if (val.contentEquals("--.-")) {
+            System.out.print(alphabet[16]);
+        }
+        if (val.contentEquals(".-.")) {
+            System.out.print(alphabet[17]);
+        }
+        if (val.contentEquals("...")) {
+            System.out.print(alphabet[18]);
+        }
+        if (val.contentEquals("-")) {
+            System.out.print(alphabet[19]);
+        }
+        if (val.contentEquals("..-")) {
+            System.out.print(alphabet[20]);
+        }
+        if (val.contentEquals("...-")) {
+            System.out.print(alphabet[21]);
+        }
+        if (val.contentEquals(".--")) {
+            System.out.print(alphabet[22]);
+        }
+        if (val.contentEquals("-..-")) {
+            System.out.print(alphabet[23]);
+        }
+        if (val.contentEquals("-.--")) {
+            System.out.print(alphabet[24]);
+        }
+        if (val.contentEquals("--..")) {
+            System.out.print(alphabet[25]);
+        }
+        if (val.contentEquals(".----")) {
+            System.out.print(nums[0]);
+        }
+        if (val.contentEquals("..---")) {
+            System.out.print(nums[1]);
+        }
+        if (val.contentEquals("...--")) {
+            System.out.print(nums[2]);
+        }
+        if (val.contentEquals("....-")) {
+            System.out.print(nums[3]);
+        }
+        if (val.contentEquals(".....")) {
+            System.out.print(nums[4]);
+        }
+        if (val.contentEquals("-....")) {
+            System.out.print(nums[5]);
+        }
+        if (val.contentEquals("--...")) {
+            System.out.print(nums[6]);
+        }
+        if (val.contentEquals("---..")) {
+            System.out.print(nums[7]);
+        }
+        if (val.contentEquals("----.")) {
+            System.out.print(nums[8]);
+        }
+        if (val.contentEquals("-----")) {
+            System.out.print(nums[9]);
+        }
+        if (val.contains("|")) {
+            System.out.print(" ");
         }
 
     }
-    public static void english_to_morse()
-    {
+
+    public static void english_to_morse() {
         /*
         The user will pass in english here
          */
 
     }
-    public static void main(String [] args)
-    {
+
+    public static void main(String[] args) {
         // Create a scanner
         System.out.println("Select 1 for Morse to English or 2 for English to Morse:");
         Scanner user_selection = new Scanner(System.in);
         int user_input = user_selection.nextInt();
         System.out.println(user_input);
-        switch (user_input)
-        {
-            case 1:
-            {
+        switch (user_input) {
+            case 1: {
                 System.out.println("You selected Morse to English!");
                 System.out.println();
                 System.out.println("Type in your sentence in Morse below:");
                 Scanner user_morse = new Scanner(System.in);
                 String morse_input = user_morse.nextLine();
                 //System.out.println(morse_input);
-                String in[] = morse_input.split(" ");    
-                for(int i=0;i<in.length;i++)
-                {
+                String in[] = morse_input.split(" ");
+                for (int i = 0; i < in.length; i++) {
                     //System.out.print("The length of the input is " + in.length);
                     morse_to_english(in[i]);
                 }
                 break;
             }
-            case 2:
-            {
+            case 2: {
                 System.out.println("You selected English to Morse!");
                 System.out.println();
                 System.out.println("Type in your sentence in english below:");
@@ -70,8 +168,7 @@ public class morseCodeBuilder
                 String english_input = user_english.nextLine();
                 char eng[] = new char[english_input.length()];
                 System.out.println(english_input);
-                for(int i=0;i<english_input.length();i++)
-                {
+                for (int i = 0; i < english_input.length(); i++) {
                     System.out.println(english_input.charAt(i));
                     eng[i] = english_input.charAt(i);
                 }
@@ -82,8 +179,7 @@ public class morseCodeBuilder
 
                 break;
             }
-            default:
-            {
+            default: {
                 System.out.println("That wasn't an option.");
                 break;
             }
